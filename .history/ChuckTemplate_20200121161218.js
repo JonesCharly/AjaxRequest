0f9abaf58ad7e66fc2ce9697986e68319ff27e90
Charline Fazentieux <charline.fazentieux@ekino.com>
2020-01-21 16:18:17 +0100

    first commit

---
+++ .history/ChuckTemplate_20200121161218.js
@@ -0,0 +1,21 @@
+function fetchChuckJSON() {
+  // Feel free to download this HTML and edit it, to use another Pokemon ID
+  const url = `https://api.chucknorris.io/jokes/random`;
+  axios
+    .get(url)
+    .then(function(response) {
+      return response.data; // SUBTLE difference with Fetch: response.data instead of response.json()
+    })
+    .then(function(quote) {
+      console.log("data decoded from JSON:", quote);
+
+      // Build a block of HTML
+      const quoteHtml = `
+        <p><strong>${quote.name}</strong></p>
+        <img src="${quote.value}" />
+      `;
+      document.querySelector("#quote").innerHTML = quoteHtml;
+    });
+}
+
+fetchChuckJSON();
